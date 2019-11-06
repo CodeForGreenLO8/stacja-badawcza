71c352491f8b8bf7fac15bdc58306bbfa72c4687
Randoragon <randoragongamedev@gmail.com>
2019-11-06 16:43:53 +0100

    Add iteration count to all.py
    Finally managed to get a reading from PMS!!!

--- sensors/PMS5003/Old/examples/all.py
+++ sensors/PMS5003/Old/examples/all.py
@@ -9,10 +9,13 @@ Press Ctrl+C to exit!
 """)
 
 pms5003 = PMS5003()
+i = 1
 
 try:
     while True:
         data = pms5003.read()
+        print(i)
+        i += 1
         print(data)
 
 except KeyboardInterrupt:
