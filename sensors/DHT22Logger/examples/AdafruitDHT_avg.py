7543bc56ecd57c61820c746fc482c119d38092c1
Randoragon <randoragongamedev@gmail.com>
2019-11-18 17:52:54 +0100

    Finish main functionality of averaging scripts
    Results are pretty great when running master.py in debug mode.
    
    - each reading is protected with a try/except block,
    - for each reading the values are checked, if "None", then a reading is
    retried up to 15 times. If 15 times pass, that reading is dropped and
    the reading count is reduced, thus the average shouldn't suffer from
    temporary malfunctions.
    - If all readings fail, the script raises a RuntimeException and doesn't
    touch the database at all

--- sensors/DHT22Logger/examples/AdafruitDHT_avg.py
+++ sensors/DHT22Logger/examples/AdafruitDHT_avg.py
@@ -35,24 +35,47 @@ else:
     sys.exit(1)
 
 # Read sensor data multiple times and make an average
-READING_COUNT = 10
-humidity      = 0.0
-temperature   = 0.0
+read_count  = 10
+RETRY_MAX   = 15
+humidity    = 0.0
+temperature = 0.0
 date1 = datetime.datetime.now()
-print('{} | DHT22: {} readings pending...'.format(date1, READING_COUNT))
-for i in range(READING_COUNT):
-    date_str = '{}-{}-{}'.format(str(date.year), str(date.month), str(date.day))
-    time_str = '{}:{}:{}'.format(str(date.hour), str(date.minute), str(date.second))
+print('{} | DHT22: {} readings pending...'.format(date1, read_count))
+# Each reading has up to RETRY_MAX chances to fail and be restarted
+for i in range(read_count):
+    retry_count = 0
     # Try to grab a sensor reading. Use the read_retry method which will retry up
     # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-    h, t= Adafruit_DHT.read_retry(sensor, pin)
-    humidity += h
-    temperature += t
+    while retry_count < RETRY_MAX:
+        try:
+            h, t = Adafruit_DHT.read(sensor, pin)
+            if (h == None or t == None):
+                raise ValueError('DHT22 Adafruit_DHT.read() failed!')
+            print('#{}\n\thumidity: {}\n\ttemperature: {}'.format(i+1, h, t))
+            humidity += h
+            temperature += t
+            break
+        except Exception as exception:
+            print('Caught exception: \'{}\'. DHT22 reading failed.'.format(type(exception).__name__))
+            retry_count += 1
+            if retry_count < RETRY_MAX:
+                print('Retrying... x{}'.format(retry_count))
+            else:
+                # Reading failed after all retries, reduce read_count
+                read_count -= 1
+            sleep(1)
     sleep(1)
-humidity    /= READING_COUNT
-temperature /= READING_COUNT
-date2 = datetime.datetime.now()
-print('{} | DHT22: Finished.'.format(date2))
+
+if read_count > 0:
+    humidity    /= read_count
+    temperature /= read_count
+    date2 = datetime.datetime.now()
+    print('{} | DHT22: Finished.'.format(date2))
+    date_str = '{}-{}-{}'.format(str(date2.year), str(date2.month), str(date2.day))
+    time_str = '{}:{}:{}'.format(str(date2.hour), str(date2.minute), str(date2.second))
+else:
+    raise RuntimeError('DHT22 sensor critical failure!')
+    exit()
 
 db = mysql.connector.connect(
      host   = mysql_info['ip'],     # your host, usually localhost
--- sensors/PMS5003/Old/examples/pm25pms5003_avg.py
+++ sensors/PMS5003/Old/examples/pm25pms5003_avg.py
@@ -11,6 +11,7 @@ import sys
 import mysql.connector
 import datetime
 from pms5003 import PMS5003
+from time import sleep
 
 if len(sys.argv) == 7:
     mysql_info = {
@@ -26,16 +27,56 @@ else:
     print('Example: python pms5003.py 10.0.0.172 3306 user pass db table')
     sys.exit(1)
 
-# Get a reading
+# Read sensor data multiple times and make an average
 pms5003 = PMS5003()
-try:
-    data = pms5003.read()
-    pm1  = data.pm_ug_per_m3(1)
-    pm10 = data.pm_ug_per_m3(10)
-    pm25 = data.pm_ug_per_m3(2.5)
-except Exception as exception:
-    print('Caught exception: \'{}\'. PMS5003 reading failed.'.format(type(exception).__name__))
+read_count  = 10
+RETRY_MAX   = 15
+pm1  = 0.0
+pm10 = 0.0
+pm25 = 0.0
+date1 = datetime.datetime.now()
+print('{} | PMS5003: {} readings pending...'.format(date1, read_count))
+# Each reading has up to RETRY_MAX chances to fail and be restarted
+for i in range(read_count):
+    retry_count = 0
+    # Try to grab a sensor reading. Use the read_retry method which will retry up
+    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+    while retry_count < RETRY_MAX:
+        try:
+            data = pms5003.read()
+            p1  = data.pm_ug_per_m3(1)
+            p10 = data.pm_ug_per_m3(10)
+            p25 = data.pm_ug_per_m3(2.5)
+            if (p1 == None or p10 == None or p25 == None):
+                raise ValueError('PMS5003 pms5003.read() failed!')
+            print('#{}\n\tpm1: {}\n\tpm10: {}\n\tpm25: {}'.format(i+1, p1, p10, p25))
+            pm1  += p1
+            pm10 += p10
+            pm25 += p25
+            break
+        except Exception as exception:
+            print('Caught exception: \'{}\'. PMS5003 reading failed.'.format(type(exception).__name__))
+            retry_count += 1
+            if retry_count < RETRY_MAX:
+                print('Retrying... x{}'.format(retry_count))
+            else:
+                # Reading failed after all retries, reduce read_count
+                read_count -= 1
+            sleep(1)
+    sleep(1)
+
+if read_count > 0:
+    pm1  /= read_count
+    pm10 /= read_count
+    pm25 /= read_count
+    date2 = datetime.datetime.now()
+    print('{} | PMS5003: Finished.'.format(date2))
+    date_str = '{}-{}-{}'.format(str(date2.year), str(date2.month), str(date2.day))
+    time_str = '{}:{}:{}'.format(str(date2.hour), str(date2.minute), str(date2.second))
+else:
+    raise RuntimeError('PMS5003 sensor critical failure!')
     exit()
+
 
 db = mysql.connector.connect(
      host   = mysql_info['ip'],     # your host, usually localhost
@@ -53,7 +94,7 @@ date_str = '{}-{}-{}'.format(str(date.year), str(date.month), str(date.day))
 time_str = '{}:{}:{}'.format(str(date.hour), str(date.minute), str(date.second))
 
 # Print the reading to stdout
-print('{}:\n\tpm1: {}\n\tpm10: {}\n\tpm25: {}'.format(date, pm1, pm10, pm25))
+print('pm1: {}\npm10: {}\npm25: {}'.format(pm1, pm10, pm25))
 
 # Write the information to the database
 sql = "INSERT INTO {} (date, time, pm1, pm25, pm10) VALUES (%s, %s, %s, %s, %s);".format(mysql_info['table'])
