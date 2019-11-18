b29c5a1b2e402bb085c39aa170977e76b0758b6c
Randoragon <randoragongamedev@gmail.com>
2019-11-18 15:37:31 +0100

    Refactor master.py into two files, add .gitignore
    - moved the Script class from master.py to scripthandler.py,
    - the Script class now takes a single ctor arg for mysql credentials,
    which gets rid of the awkward SQL_CREDENTIALS variable dependency,
    - added .gitignore to exclude __pycache__ folders from ./sensors/

---
+++ sensors/scripthandler.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import re
+import os
+
+class Script:
+    def __init__(self, name, args, mysql, command='python'):
+        self.name    = name
+        self.args    = args
+        self.mysql   = mysql
+        self.command = command
+        
+    def call(self):
+        cmd = self.concat_callable()
+        try:
+            output = os.system(cmd)
+        except Exception as exception:
+            print('Exception occurred for \'{}\': \'{}\'. Reading failed.'.format(cmd, type(exception).__name__))
+            return -1
+        return output
+
+    def concat_callable(self):
+        return '{} {} {} {}'.format(self.command, self.name, self.args, self.mysql)
+        
+    def __repr__(self):
+        return """
+{}(
+script: {},
+  exec: {},
+ mysql: {},
+   cmd: {}
+)
+""".format(re.search(r'/([^/]*)$', self.name).group(1), self.command, self.name, self.args, self.mysql, self.concat_callable())
+
